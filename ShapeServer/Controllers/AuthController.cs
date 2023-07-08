a309a72e5b393cc9c2fdb2864cd422d72a6670ea
Taseer <taseer.ahmadd@gmail.com>
2023-07-08 12:47:26 +0500

    Create Signup route with DTO

---
+++ ShapeServer/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShapeServer.DTO.SignupRequest;
+
+namespace ShapeServer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("Signup")]
+        public async Task<IActionResult> Signup(SignupRequestDto requestDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ShapeServer/DTO/SignupRequest/SignupRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace ShapeServer.DTO.SignupRequest
+{
+    [BindRequired]
+    public record SignupRequestDto(
+        string FirstName,
+        string LastName,
+        string Email,
+        string Password,
+        string ConfirmPassword
+        );
+}
