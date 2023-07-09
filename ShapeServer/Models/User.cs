b77c648178d8e767a352074f779f6add61b18f8a
Taseer <taseer.ahmadd@gmail.com>
2023-07-09 14:06:17 +0500

    Updated Id type to long

--- ShapeServer/Models/User.cs
+++ ShapeServer/Models/User.cs
@@ -2,7 +2,7 @@
 {
     public class User
     {
-        public int Id { get; set; }
+        public long Id { get; set; }
         public required string Email { get; set; }
         public required string FirstName { get; set; }
         public required string LastName { get; set; }
